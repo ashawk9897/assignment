9f4998392a3b7c99af7df2326c2a333a7055ce10
ashokinsta <ashok@instachk.today>
2019-04-25 16:09:12 +0530

    changes base url, updated readme

--- assignment/urls.py
+++ assignment/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from collapse import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('collapse/', views.collapse,name='collapse'),
+    path('', views.collapse,name='collapse'),
 ]
