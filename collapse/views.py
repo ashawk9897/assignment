3fe288038e4a09e6392637c61d84f1fbe68fa163
ashokinsta <ashok@instachk.today>
2019-04-25 20:02:08 +0530

    changes to handle '.' as indent as per the input file

--- collapse/views.py
+++ collapse/views.py
@@ -5,6 +5,7 @@ def collapse(request):
     modified_text=''
     template = 'index.html'
     file=''
+    indenter='.'
     if request.method=='POST':
         try:
             file = request.FILES['file']
@@ -18,23 +19,63 @@ def collapse(request):
         next_space=0
         next_line=''
         adder='-'
+        line_number=1
+
         for line in data.splitlines():
+            # print('----------------------------------------------')
+            # print(line,previous_space,next_space)
+            if line.startswith('*'):
+                star_count=len(line)-len(line.lstrip('*'))
+                if star_count>1:
+                    str_line_number = str(line_number)
+                    for i in range(1,star_count):
+                        sub_line_number = 1
+                        str_line_number=str_line_number+'.'+str(sub_line_number)
+                    sub_line_number += 1
+                else:
+                    str_line_number=str(line_number)
+                    line_number += 1
+                if previous_space != 0:
+                    modified_text.append(' ' * (previous_space+1) + '-' + previous_line.lstrip(indenter))
+                modified_text.append(str_line_number + line[1:])
+
+                previous_line=''
+                previous_space=0
+                next_space = 0
+                continue
+
+            if not len(line.lstrip()):
+                #print('continue')
+                continue
             if previous_line=='':
                 previous_line=line
-                previous_space = len(previous_line) - len(previous_line.lstrip())
+                previous_space = len(previous_line) - len(previous_line.lstrip(indenter))
                 continue
-            next_space=len(line) -len(line.lstrip())
+            if line.startswith('.'):
+                next_space = len(line) - len(line.lstrip(indenter))
             if(previous_space<next_space):
-                adder='+'
-                modified_text.append(adder+previous_line)
+                adder = '+'
+                if previous_space==0:
+                    previous_line = line
+                    previous_space = next_space
+                    continue
+                modified_text.append(' '* (previous_space+1) +adder+previous_line.lstrip(indenter))
             else:
-
-                previous_space = len(previous_line) - len(previous_line.lstrip())
-                modified_text.append(adder + previous_line )
+                adder = "-"
+                previous_space = len(previous_line) - len(previous_line.lstrip(indenter))
+                modified_text.append(' '* (previous_space+1) +adder +previous_line.lstrip(indenter) )
+            # else:
+            #     modified_text.append(' '*previous_space+previous_line.lstrip(indenter))
             previous_line=line
             previous_space=next_space
             adder = "-"
-        modified_text.append(adder+previous_line)
+        if previous_line.startswith('.'):
+            indenter = '.'
+        else:
+            indenter = ' '
+        #print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',previous_line,indenter,'aaaaaaa',previous_line.lstrip(indenter))
+        #print(' '* (previous_space+1) +adder+previous_line.lstrip(indenter))
+        modified_text.append(' '* (previous_space+1) +adder+previous_line.lstrip(indenter))
         modified_text='\n'.join(modified_text)
 
     return render(request,template_name=template,context={'modified_text':modified_text,'file':file})
